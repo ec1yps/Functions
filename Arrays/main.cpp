698ab783f22d2f175b72466800f1f63619fee6c6
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-04-16 21:21:19 +0300

    SumOverloading

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,9 +6,16 @@ using std::endl;
 #define tab "\t"
 
 void FillRand(int arr[], const int n);
-void Print(const int arr[], const int n);
+void FillRand(double arr[], const int n);
+
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+
 void Sort(int arr[], const int n);
+
 int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+
 double Avg(int arr[], const int n);
 int minValueIn(int arr[], const int n);
 int maxValueIn(int arr[], const int n);
@@ -28,13 +35,22 @@ void main()
 	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
 	cout << "Минимальное значение массива: " << minValueIn(arr, n) << endl;
 	cout << "Максимальное значение массива: " << maxValueIn(arr, n) << endl;
-	int number_of_shifts;
-	cout << "Введите количество сдвигов влево: "; cin >> number_of_shifts;
-	shiftLeft(arr, n, number_of_shifts);
-	Print(arr, n);
-	cout << "Введите количество сдвигов вправо: "; cin >> number_of_shifts;
-	shiftRight(arr, n, number_of_shifts);
-	Print(arr, n);
+	//int number_of_shifts;
+	//cout << "Введите количество сдвигов влево: "; cin >> number_of_shifts;
+	//shiftLeft(arr, n, number_of_shifts);
+	//Print(arr, n);
+	//cout << "Введите количество сдвигов вправо: "; cin >> number_of_shifts;
+	//shiftRight(arr, n, number_of_shifts);
+	//Print(arr, n);
+
+	const int SIZE = 8;
+	double brr[SIZE];
+	FillRand(brr, SIZE);
+	Print(brr, SIZE);
+	cout << "Сумма элементов массива: " << Sum(brr, SIZE) << endl;
+	//cout << "Среднее-арифметическое элементов массива: " << Avg(brr, SIZE) << endl;
+	//cout << "Минимальное значение массива: " << minValueIn(brr, SIZE) << endl;
+	//cout << "Максимальное значение массива: " << maxValueIn(brr, SIZE) << endl;
 }
 
 void FillRand(int arr[], const int n)
@@ -44,8 +60,23 @@ void FillRand(int arr[], const int n)
 		arr[i] = rand() % 100;
 	}
 }
+void FillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
 
-void Print(const int arr[], const int n)
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Print(double arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -71,6 +102,15 @@ void Sort(int arr[], const int n)
 }
 
 int Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Sum(double arr[], const int n)
 {
 	int sum = 0;
 	for (int i = 0; i < n; i++)
